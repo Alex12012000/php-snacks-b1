c4c584137bfab9f29107a2f0f16ff2c015b419dc
Alex12012000 <arcex.29@gmail.com>
2022-07-22 15:41:20 +0200

    Snack 4

---
+++ Snack4/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $p = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero iusto et earum porro fugit. eos quasi explicabo nihil. iure dolorem neque voluptas maxime sit quis quae harum. ipsum reprehenderit consequatur?';
+
+    var_dump(explode('.', $p));
+
+
+?>
+
